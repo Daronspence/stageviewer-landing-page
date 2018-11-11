9566c1fed51968f9b5e9851a8d0d212d97adc78f
Daron Spence <daron@weare502.com>
2018-11-11 13:26:45 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="min-h-screen flex flex-col items-center justify-center bg-blue-darkest p-8">
+    <div class="w-sm max-w-sm bg-white p-8 sm:py-16">
+      <h1 class="text-3xl md:text-4xl mb-4">Stage Viewer</h1>
+      <p class="mb-2 text-left">Stage Viewer helps you visualize your church's stage layout.</p>
+      <p class="mb-2 text-left">Integrate with Planning Center to organize your band and worship team while providing clarity to your Band, Front of House and Media teams.</p>
+      <a href='#learn-more' class="m-2 inline-block" @click.once="learnMoreOpen = true" v-if="!learnMoreOpen">Learn More</a>
+      <div id="learn-more" class="mt-8">
+        <div v-if="learnMoreOpen" class="mb-8 text-left text-sm">
+          <hr class="bg-grey-light h-1 w-full my-8">
+          <p>Stage Viewer aims to fill the gap for a few common problems that churches face every week. Knowing who is on stage and where seems like a simple problem to solve. Unfortunately, we struggle with this every week!</p>
+          <p>Imagine an audio engineer is a back room mixing the worship service in real time on a separate console. Without a live video feed, they can't see who is on stage and what mic they are using. Singers can change every week, making it hard to label the board with static names.</p>
+          <p>Worship teams with a large pools of singers often swap mics on a weekly basis. Wouldn't it be great if they could see which mic and pack they were assigned to on a screen above the mic cabinet?</p>
+          <p>Stage Viewer can help with all of these scenarios and more, by allowing you to create a virtual representation of your stage layout every week, accessbile online with a simple shareable URL.</p>
+        </div>
+      </div>
+      <h2 class="mb-4">Interested?</h2>
+      <SignUp/>
+      <a class="inline-block mt-8" href="#feedback" @click="submitFeedback">Got Feedback?</a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SignUp
+  },
+  data: function(){
+    return {
+      learnMoreOpen: false
+    }
+  },
+  methods: {
+    submitFeedback: function(){
+      window.open('mailto:daron' + 'spence@' + 'gmail.com?subject=Stage Viewer Feedback');
+    }
   }
 }
 </script>
 
 <style>
 #app {
+  background-image: url('./assets/band.jpg');
+  background-size: cover;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#learn-more p {
+  margin-bottom: 1rem;
+}
+
+a {
+  color: #38A89D;
 }
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="sign-up">
+    <!-- Begin Mailchimp Signup Form -->
+    <div id="mc_embed_signup" class="text-left mx-auto">
+    <form action="https://acfwidgets.us9.list-manage.com/subscribe/post?u=66c6af09ba208080a03459801&amp;id=dc648e2428" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+
+    <div class="indicates-required text-xs text-red text-left mb-1"><span class="asterisk">*</span> required</div>
+    <div class="flex mb-4 flex-col sm:flex-row items-center">
+      <label for="mce-EMAIL" class="flex-1">Email Address  <span class="asterisk text-red">*</span></label>
+      <input type="email" value="" required name="EMAIL" class="required email px-4 py-2 border" id="mce-EMAIL">
+    </div>
+    <div class="flex mb-4 flex-col sm:flex-row items-center">
+      <label for="mce-FNAME" class="flex-1">First Name </label>
+      <input type="text" value="" name="FNAME" class="px-4 py-2 border" id="mce-FNAME">
+    </div>
+    <div class="flex mb-4 flex-col sm:flex-row items-center">
+      <label for="mce-LNAME" class="flex-1">Last Name </label>
+      <input type="text" value="" name="LNAME" class="px-4 py-2 border" id="mce-LNAME">
+    </div>
+    <div id="mce-responses" class="clear">
+      <div class="response" id="mce-error-response" style="display:none"></div>
+      <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_66c6af09ba208080a03459801_dc648e2428" tabindex="-1" value=""></div>
+      <div class="text-center mt-8"><input type="submit" value="Subscribe to Updates" name="subscribe" id="mc-embedded-subscribe" class="p-2 bg-green-dark text-white"></div>
+      </div>
+    </form>
+    </div>
+    <!--End mc_embed_signup-->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
